bc8e0fa2244002f609717189b42abe15e97fb768
gianluca-moriconi <gianlucamoriconi08@gmail.com>
2025-09-26 11:39:24 +0200

    Less demential descriptions

--- src/cards/CardFactory.java
+++ src/cards/CardFactory.java
@@ -10,12 +10,12 @@ public class CardFactory { //CLASSE CARDFACTORY
         
     	return List.of( //List.of crea una lista immutabile
     			
-            new Card("Grattati (cura 2)", Phase.EXPLORATION, Set.of(), //Costruttore carta | Nome carta : Fase : Allowed=Set.of vuoto=mazzo generico
-                (g,p) -> g.heal(p, 2, "Si gratta il culo per curare 2 HP")), 	//Lambda che implementa BiConsumer<Game, Player> | 
+            new Card("Sgranchisciti (cura 2)", Phase.EXPLORATION, Set.of(), //Costruttore carta | Nome carta : Fase : Allowed=Set.of vuoto=mazzo generico
+                (g,p) -> g.heal(p, 2, "Si stiracchia per curare 2 HP")), 	//Lambda che implementa BiConsumer<Game, Player> | 
             											 	//quando giochi la carta, chiama heal sul Game passato a runtime (g) curando il player p di 2.
            
-            new Card("Stappa una Redbard (Pozioni +1)", Phase.EXPLORATION, Set.of(),
-                (g,p) -> { g.teamPotions++; System.out.println(p.name+" Redbard ti mette le aaaliii!!! Pozioni del party: "+g.teamPotions); })
+            new Card("Distilla pozione(Pozioni +1)", Phase.EXPLORATION, Set.of(),
+                (g,p) -> { g.teamPotions++; System.out.println(p.name+" Pesta e mescola! Pozioni del party: "+g.teamPotions); })
         );
     }
 
@@ -24,13 +24,13 @@ public class CardFactory { //CLASSE CARDFACTORY
         List<Card> list = new ArrayList<>();
         switch (cls) {
             case BARDO -> list.add(new Card("Assolo alternativo (+1 danno prossimo attacco)", Phase.EXPLORATION, Set.of(ClassType.BARDO),
-                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" esegue un assolo di liuto con le dita dei piedi. Bonus danno: +"+p.nextAttackBonus); }));
+                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" esegue un assolo di liuto. Bonus danno: +"+p.nextAttackBonus); }));
             case GUERRIERO -> list.add(new Card("Carica eroica (+1 danno prossimo attacco)", Phase.EXPLORATION, Set.of(ClassType.GUERRIERO),
-                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" : 'Se vieni avanti ancora ti do un pugno!!' Bonus danno: +"+p.nextAttackBonus); }));
+                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" gonfia i muscoli. Bonus danno: +"+p.nextAttackBonus); }));
             case MAGO -> list.add(new Card("Riflessione intellettuale (+1 danno prossimo attacco)", Phase.EXPLORATION, Set.of(ClassType.MAGO),
-                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" : '15 e 18 quanto fa? COGL####!! D## POR##!!'. Bonus danno: +"+p.nextAttackBonus); }));
+                (g,p) -> { p.nextAttackBonus += 1; System.out.println(p.name+" esplora i suoi pensieri. Bonus danno: +"+p.nextAttackBonus); }));
             case LADRO -> list.add(new Card("Fai scorta (Pozioni +1)", Phase.EXPLORATION, Set.of(ClassType.LADRO),
-                (g,p) -> { g.teamPotions++; System.out.println(p.name+" svaligia un minimarket con i cugini di Nap###. Pozioni: "+g.teamPotions); }));
+                (g,p) -> { g.teamPotions++; System.out.println(p.name+" sfila qualche oggetto dalle tasche dei passanti. Pozioni: "+g.teamPotions); }));
         }
         return list;
     }
@@ -39,9 +39,9 @@ public class CardFactory { //CLASSE CARDFACTORY
     public static List<Card> combatGeneric(Game G) {
         return List.of(
             new Card("Calcio basso (1 danni)", Phase.COMBAT, Set.of(),
-                (g,p) -> g.dealDamage(1, p, " un calcio basso. Meglio darlo che riceverlo!")),
+                (g,p) -> g.dealDamage(1, p, " un calcio basso")),
             new Card("Colpo rapido (2 danni)", Phase.COMBAT, Set.of(),
-                (g,p) -> g.dealDamage(2, p, " la cosiddetta 'una botta e via'"))
+                (g,p) -> g.dealDamage(2, p, " un attacco rapido"))
         );
     }
 
@@ -50,7 +50,7 @@ public class CardFactory { //CLASSE CARDFACTORY
         List<Card> list = new ArrayList<>();
         switch (cls) {
             case BARDO -> list.add(new Card("Acuto potente (+2 al prossimo attacco)", Phase.COMBAT, Set.of(ClassType.BARDO),
-                (g,p) -> { p.nextAttackBonus += 2; System.out.println(p.name+" si strizza le palle per raggiungere nuove vette! Bonus danno: +"+p.nextAttackBonus); }));
+                (g,p) -> { p.nextAttackBonus += 2; System.out.println(p.name+" si lascia andare. Bonus danno: +"+p.nextAttackBonus); }));
             case GUERRIERO -> list.add(new Card("Colpo possente (4 danni)", Phase.COMBAT, Set.of(ClassType.GUERRIERO),
                 (g,p) -> g.dealDamage(4, p, "Colpo possente")));
             case MAGO -> list.add(new Card("Dardo incantato (3 danni sicuri)", Phase.COMBAT, Set.of(ClassType.MAGO),
